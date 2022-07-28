022b89e2a5c2c0ae39ae32820888a7ac51a1766d
Vasilii <liklo.adm@gmail.com>
2022-07-28 13:08:51 +0300

    update yespower

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -126,14 +126,14 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xf1;
-        pchMessageStart[1] = 0xcf;
-        pchMessageStart[2] = 0xa6;
-        pchMessageStart[3] = 0xd3;
-        nDefaultPort = 3888;
+        pchMessageStart[0] = 0xec;
+        pchMessageStart[1] = 0xfa;
+        pchMessageStart[2] = 0xce;
+        pchMessageStart[3] = 0xa5;
+        nDefaultPort = 8755;
         nPruneAfterHeight = 100000;
-        m_assumed_blockchain_size = 16;
-        m_assumed_chain_state_size = 1;
+        m_assumed_blockchain_size = 22;
+        m_assumed_chain_state_size = 3;
 
         genesis = CreateGenesisBlock(1504695029, 8026361, 0x1f00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();

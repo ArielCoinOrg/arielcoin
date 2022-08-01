82f33065c7cc341f8edf4ff4a823ad5ea3edd88b
Vasilii <liklo.adm@gmail.com>
2022-08-01 12:48:58 +0300

    kawpow🤷‍

--- src/eth_client/utils/ethash/include/ethash/ethash.hpp
+++ src/eth_client/utils/ethash/include/ethash/ethash.hpp
@@ -14,8 +14,8 @@
 
 #pragma once
 
-#include <ethash/ethash.h>
-#include <ethash/hash_types.hpp>
+#include <eth_client/utils/ethash/include/ethash/ethash.h>
+#include <eth_client/utils/ethash/include/ethash/hash_types.hpp>
 
 #include <cstdint>
 #include <cstring>

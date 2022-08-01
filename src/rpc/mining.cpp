1d6ce533baeb4671546acbe1212757e3730c774f
Vasilii <liklo.adm@gmail.com>
2022-08-01 13:58:18 +0300

    kawpow🤷‍

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -130,7 +130,7 @@ static bool GenerateBlock(ChainstateManager& chainman, CBlock& block, uint64_t& 
 
     CChainParams chainparams(Params());
     uint256 mix_hash;
-    while (max_tries > 0 && block.nNonce < std::numeric_limits<uint32_t>::max() && !CheckProofOfWork(block.GetHash(mix_hash), block.nBits, chainparams.GetConsensus()) && !ShutdownRequested()) {
+    while (max_tries > 0 && block.nNonce < std::numeric_limits<uint32_t>::max() && !CheckProofOfWork(block.GetHashFull(mix_hash), block.nBits, chainparams.GetConsensus()) && !ShutdownRequested()) {
         ++block.nNonce;
         --max_tries;
     }

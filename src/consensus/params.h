b7f0edd0a64098d1ca6329bd956e04edc9432464
Vasilii <liklo.adm@gmail.com>
2022-08-16 15:18:09 +0300

    first try to delete root🤷‍

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -99,6 +99,7 @@ struct Params {
     int QIP9Height;
     /** Block height at which Offline Staking becomes active */
     int nOfflineStakeHeight;
+    int nSmartActivationBlock;
     /** Block height at which Reduce Block Time becomes active */
     int nReduceBlocktimeHeight;
     /** Block height at which EVM Muir Glacier fork becomes active */

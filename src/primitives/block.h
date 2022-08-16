095d2cbb7947b2a2b3d164e806b9f23e52766a8b
Vasilii <liklo.adm@gmail.com>
2022-08-16 16:05:18 +0300

    first try to delete root🤷‍

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -57,7 +57,7 @@ public:
         READWRITE(obj.nHeight);
         READWRITE(obj.nNonce64);
         READWRITE(obj.mix_hash);
-        if (nSmartActivationBlock < obj.nHeight){
+        if (nSmartActivationBlock <= obj.nHeight){
             READWRITE(obj.hashStateRoot);
             READWRITE(obj.hashUTXORoot);
             READWRITE(obj.prevoutStake);

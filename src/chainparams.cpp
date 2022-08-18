7b1a1bd6005abfc96cec373e8bd8859361ca635d
Vasilii <liklo.adm@gmail.com>
2022-08-18 15:29:04 +0300

    fix root костыль🤷‍

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -198,7 +198,6 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = Consensus::BIP9Deployment::ALWAYS_ACTIVE;
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = Consensus::BIP9Deployment::NO_TIMEOUT;
 
-
         consensus.nMinimumChainWork = uint256S("0x01"); // 1883974
         consensus.defaultAssumeValid = uint256S(""); // 1883974
 

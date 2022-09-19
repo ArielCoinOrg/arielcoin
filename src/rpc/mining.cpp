9be8d5adaae8d13bc8a1ae9fc1e2eccfbbcff9f4
Vasilii <liklo.adm@gmail.com>
2022-09-19 09:26:26 +0300

    fix qtum -> ariel

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -1105,8 +1105,8 @@ static RPCHelpMan getblocktemplate()
     result.pushKV("height", (int64_t)(pindexPrev->nHeight+1));
 
     if (pindexPrev->nHeight+1 >= consensusParams.nSmartActivationBlock) {
-//        result.pushKV("hashStateRoot", HexStr(consensusParams.signet_challenge));
-//        result.pushKV("hashUTXORoot", HexStr(consensusParams.signet_challenge));
+        result.pushKV("hashStateRoot", uint256(h256Touint(dev::h256(globalState->rootHash()))).ToString());
+        result.pushKV("hashUTXORoot", uint256(h256Touint(dev::h256(globalState->rootHashUTXO()))).ToString());
     }
 
     if (consensusParams.signet_blocks) {
